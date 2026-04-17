30a9fdec8464529081918b8bd4bb92aa3b0ef78b
Diego <diegojm9823@gmail.com>
2026-04-17 02:28:31 -0300

    Fix: servir media en produccion, incluir carpeta media en git

--- ditaboutique/urls.py
+++ ditaboutique/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 """
 URL configuration for ditaboutique project.
 
@@ -22,6 +23,5 @@ Including another URLconf
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
