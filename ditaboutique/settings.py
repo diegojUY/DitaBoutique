123c9a64ab9a1ee29f15af25126f7975facd8687
Diego <diegojm9823@gmail.com>
2026-04-17 02:16:56 -0300

    Fix: buscar DATABASE_URL y DATABASE_PUBLIC_URL, quitar start.sh

--- ditaboutique/settings.py
+++ ditaboutique/settings.py
@@ -81,11 +81,19 @@ WSGI_APPLICATION = 'ditaboutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL') or os.environ.get('DATABASE_PUBLIC_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
