53e25bbc1a6e8f6e75ea04b3bb92b47e71444d2d
Diego <diegojm9823@gmail.com>
2026-04-17 01:59:12 -0300

    Configurar DATABASE_URL PostgreSQL Railway

--- ditaboutique/settings.py
+++ ditaboutique/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'ditaboutique.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////' + str(BASE_DIR / 'db.sqlite3')
+        default='postgresql://postgres:lpyKxkdPRMgIUlIKZxEqlwbEIFznnRjj@postgres.railway.internal:5432/railway'
     )
 }
 
